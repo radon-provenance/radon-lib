d1069ec38fcb2be4e20be7ab07ace4bd2afb8174
JeromeFuselier <jerome.fuselier@free.fr>
2020-05-14 23:22:55 +0200

    Add some automatic creation for testing

--- tests/create_test_env.py
+++ tests/create_test_env.py
@@ -22,13 +22,25 @@ from radon.models import (
     sync
 )
 
-from radon.models.errors import GroupConflictError
+from radon.models.errors import (
+    GroupConflictError,
+    UserConflictError
+)
 from radon.models.group import Group
+from radon.models.user import User
 from radon.models.collection import Collection
+from radon.models.data_object import DataObject
+from radon.models.resource import Resource
 
 TEST_KEYSPACE = "radon_pytest"
 
 GROUPS = ["grp1", "grp2", "grp3", "grp4"]
+
+USERS = [("user1", "user1@test.com", "user1", True, ["grp1"]),
+         ("user2", "user2@test.com", "user2", False, ["grp2"]),
+         ("user3", "user3@test.com", "user3", False, ["grp1", "grp2"]),
+         ("user4", "user4@test.com", "user4", False, ["grp3"])]
+
 
 def create_keyspace():
     """Create a set of objects in a radon keyspace created for the test
@@ -40,6 +52,7 @@ def create_keyspace():
     # Create tables
     sync()
 
+
 def create_groups():
     for group_name in GROUPS:
         try:
@@ -48,24 +61,72 @@ def create_groups():
         except GroupConflictError:
             print ("group '%s' already exists" % (group_name))
 
+
+def create_users():
+    for (user_name, email, password, administrator, groups) in USERS:
+        try:
+            user = User.create(name=user_name, email=email,
+                               password=password, administrator=administrator,
+                               groups=groups)
+            print ("user '%s' created (uuid='%s')" % (user.name, user.uuid))
+        except UserConflictError:
+            print ("user '%s' already exists" % (user_name))
+
+
+def create_collection(parent, collection):
+    try:
+        Collection.create(parent, collection)
+    except: # Collection already exists quite likely
+        print("collection {}/{} already exists".format(parent, collection))
+
+
+def create_root():
+    try:
+        Collection.create_root()
+    except: # Collection already exists quite likely
+        print("Root collection already exists")
+
+
 def create_collections():
-    Collection.create_root()
-    Collection.create("coll1", "/")
-    Collection.create("coll11", "/coll1")
-    Collection.create("coll12", "/coll1")
-    Collection.create("coll13", "/coll1")
-    Collection.create("coll2", "/")
-    Collection.create("coll21", "/coll2")
-    Collection.create("coll22", "/coll2")
-    Collection.create("coll23", "/coll2")
-    Collection.create("coll3", "/")
-    Collection.create("coll31", "/coll3")
-    Collection.create("coll32", "/coll3")
-    Collection.create("coll33", "/coll3")
+    create_root()
+    create_collection("/", "coll1")
+    create_collection("/coll1","coll11")
+    create_collection("/coll1","coll12")
+    create_collection("/coll1","coll13")
+    create_collection("/", "coll2")
+    create_collection("/coll2","coll21")
+    create_collection("/coll2","coll22")
+    create_collection("/coll2","coll23")
+    create_collection("/", "coll3")
+    create_collection("/coll3","coll31")
+    create_collection("/coll3","coll32")
+    create_collection("/coll3","coll33")
+
+
+def create_resource(container, name, data):
+    try:
+        data_object = DataObject.create(data.encode())
+        uuid = data_object.uuid
+        url = "cassandra://{}".format(uuid)
+        resource = Resource.create(
+            name=name, 
+            container=container,
+            url=url,
+            size=len(data)
+        )
+    except:
+        print("resource {}/{} already exists".format(container, name))
+
+def create_resources():
+    create_resource("/", "test.txt", "test content")
+    create_resource("/coll1", "test.txt", "test content in ./coll1")
+    create_resource("/coll3/coll33", "test.txt", "test content in ./coll3/coll33")
 
     
 if __name__ == "__main__":
     create_keyspace()
     create_groups()
+    create_users()
     create_collections()
+    create_resources()
     
